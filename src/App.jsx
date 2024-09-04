0229140e9e00c87dae2e337da5ff957b46bedf7b
Adeoluwa Gbenro <joelayomide35@gmail.com>
2024-09-04 10:27:04 +0100

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Hero from './Hero'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+
+    <Hero />
+    
+    </>
+  )
+}
+
+export default App
